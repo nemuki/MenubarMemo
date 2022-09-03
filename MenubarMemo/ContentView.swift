c503bd083fc2f65b4c201ce906f023b34e12df75
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-09-03 09:06:59 +0900

    Initial Commit

---
+++ MenubarMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenubarMemo
+//
+//  Created by Naoki Takahashi on 2022/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenubarMemo/MenubarMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenubarMemoApp.swift
+//  MenubarMemo
+//
+//  Created by Naoki Takahashi on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct MenubarMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
